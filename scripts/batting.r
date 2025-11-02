eefcf431d6b51c900a5ffbec2332cf11700d5b86
Peter <petecht66@gmail.com>
2025-11-02 15:27:24 -0500

    creating baserunning and batting models

--- scripts/batting.r
+++ scripts/batting.r
@@ -1,2 +1,15 @@
 # Libraries needed for this script
 library(ggplot2)
+
+# creating batting data frame
+batting <- read.csv("https://raw.githubusercontent.com/petecht66/SPAX212-data-visualization-challenge/refs/heads/main/data/BattingData.csv")
+summary(batting)
+head(batting)
+
+# create a model to evaluate which hitting metrics are important for wins
+battingModel <- lm(W ~ HR + BA + SLG + OBP, data = batting)
+summary(battingModel)
+
+# create a model to evaluate importance of baserunning for runs scored
+baserunningModel <- lm(R ~ SB + CS, data = batting)
+summary(baserunningModel)
--- scripts/fielding.r
+++ scripts/fielding.r
@@ -4,3 +4,16 @@ library(ggplot2)
 # creating fielding data frame
 fielding <- read.csv("https://raw.githubusercontent.com/petecht66/SPAX212-data-visualization-challenge/refs/heads/main/data/FieldingData.csv")
 summary(fielding)
+head(fielding)
+
+# scatter plot between fielding percentage and runs allowed per game
+ggplot(data = fielding, aes(x = Fld., y = RA.G)) + 
+    geom_point(color = "azure4", alpha = 0.7) +
+    geom_smooth(method = "lm", color = "steelblue") + 
+    labs(
+    title = "Team Fielding Percentage vs. Runs Allowed Per Game", 
+    x = "Fielding Percentage", 
+    y = "Runs Allowed Per Game")
+
+# correlation between fielding and runs allowed, use of . in place of / and % to fix errors
+round(cor(fielding$RA.G, fielding$Fld., use="complete.obs"), 2)
